eb7ea67fef15bcf0e4e41170b458794288b4fba5
yasin-arft <yasinarftsocial@gmail.com>
2024-04-13 11:39:51 +0600

    footer added

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,11 +1,17 @@
+import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div className="container mx-auto px-3 font-poppins text-dark">
-      <Navbar />
-      <Outlet />
+    <div className="font-poppins text-dark">
+      <div className="container mx-auto px-3">
+        <Navbar />
+        <Outlet />
+      </div>
+      <div className=" bg-royal-blue text-white mt-5">
+        <Footer />
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaInstagram, FaTwitter, FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="container mx-auto px-3">
+      <footer className="footer footer-center p-16 rounded">
+        <nav className="grid grid-flow-col gap-4">
+          <Link className="link link-hover" to={'/'}>Home</Link>
+          <Link className="link link-hover" to={'/update_profile'}>Update Profile</Link>
+          <Link className="link link-hover" to={'/login'}>Login</Link>
+          <Link className="link link-hover" to={'/register'}>Register</Link>
+        </nav>
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <a><FaFacebookF className="text-xl cursor-pointer" /></a>
+            <a><FaInstagram className="text-xl cursor-pointer" /></a>
+            <a><FaTwitter className="text-xl cursor-pointer" /></a>
+            <a><FaGithub className="text-xl cursor-pointer" /></a>
+          </div>
+        </nav>
+        <aside>
+          <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        </aside>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
