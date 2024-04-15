709f88a587dd1ce0ab8ff97bd88b212ef45dcd41
yasin-arft <yasinarftsocial@gmail.com>
2024-04-15 16:39:30 +0600

    user observer set up

--- src/Providers/AuthContextProvider.jsx
+++ src/Providers/AuthContextProvider.jsx
@@ -1,11 +1,14 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import PropTypes from 'prop-types';
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import auth from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
 
 const AuthContextProvider = ({ children }) => {
+  // current user state
+  const [currentUser, setCurrentUser] = useState(null);
+
   // create user 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -15,8 +18,15 @@ const AuthContextProvider = ({ children }) => {
   const loginUser = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   }
+
+  // observer
+  onAuthStateChanged(auth, (user) => {
+    setCurrentUser(user);
+  });
+
   // auth value
   const AuthInfo = {
+    currentUser,
     createUser,
     loginUser
   }
